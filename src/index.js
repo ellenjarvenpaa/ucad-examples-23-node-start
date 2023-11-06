827a1b451f3aa519931381e04b9e1f6810d8b32e
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2023-11-06 20:23:36 +0200

    express

--- src/index.js
+++ src/index.js
@@ -1,57 +1,65 @@
-import express from 'express';
-import path from 'path';
-import {fileURLToPath} from 'url';
-import {getItems, getItemsById, postItem} from './items.js';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import {
+  getItems,
+  getItemsById,
+  postItem,
+  deleteItem,
+  putItem,
+} from "./items.js";
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const app = express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('view engine', 'pug');
-app.set('views', 'src/views');
+app.set("view engine", "pug");
+app.set("views", "src/views");
 
 app.use(express.json());
-app.use('/docs', express.static(path.join(__dirname, '../docs')));
+app.use("/docs", express.static(path.join(__dirname, "../docs")));
 
 // simple custom middleware for loggin/debugging all requests
 app.use((req, res, next) => {
-  console.log('Time:', Date.now(), req.method, req.url);
+  console.log("Time:", Date.now(), req.method, req.url);
   next();
 });
 
-
-app.get('/', (req, res) => {
-  const values = {title: "Dummy REST API docs", message: "TODO: docs"};
-  res.render('home', values);
+app.get("/", (req, res) => {
+  const values = {
+    title: "Dummy REST API for media",
+    message: "Media will be displayed here",
+  };
+  res.render("home", values);
 });
 
 // dummy routing example
-app.get('/kukkuu', (request, response) => {
-  const myResponse = {message: "No moro!"};
+app.get("/kukkuu", (request, response) => {
+  const myResponse = { message: "No moro!" };
   //response.json(myResponse);
   response.sendStatus(200);
 });
 
 // other dummy pug example
-app.get('/:message', (req, res) => {
-  const values = {title: "Dummy REST API docs", message: req.params.message};
-  res.render('home', values);
+app.get("/:message", (req, res) => {
+  const values = { title: "Dummy REST API docs", message: req.params.message };
+  res.render("home", values);
 });
 
 // example generic items api
 
 // get all items
-app.get('/api/items', getItems);
+app.get("/api/media", getItems);
 // get items by id
-app.get('/api/items/:id', getItemsById);
+app.get("/api/media/:id", getItemsById);
 // modify
-app.put('/api/items');
+app.put("/api/media", putItem);
 // add new item
-app.post('/api/items', postItem);
+app.post("/api/media", postItem);
 // remove existing item
-app.delete('/api/items');
+app.delete("/api/media", deleteItem);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- src/items.js
+++ src/items.js
@@ -1,54 +1,140 @@
-// mock items data
-const items = [
-  {id: 5, name: 'porkkana'},
-  {id: 6, name: 'omena'},
-  {id: 19, name: 'appelsiini'},
+const mediaItems = [
+  {
+    media_id: 9632,
+    filename: "ffd8.jpg",
+    filesize: 887574,
+    title: "Favorite drink",
+    description: "",
+    user_id: 1606,
+    media_type: "image/jpeg",
+    created_at: "2023-10-16T19:00:09.000Z",
+  },
+  {
+    media_id: 9626,
+    filename: "dbbd.jpg",
+    filesize: 60703,
+    title: "Miika",
+    description: "My Photo",
+    user_id: 3671,
+    media_type: "image/jpeg",
+    created_at: "2023-10-13T12:14:26.000Z",
+  },
+  {
+    media_id: 9625,
+    filename: "2f9b.jpg",
+    filesize: 30635,
+    title: "Aksux",
+    description: "friends",
+    user_id: 260,
+    media_type: "image/jpeg",
+    created_at: "2023-10-12T20:03:08.000Z",
+  },
+  {
+    media_id: 9592,
+    filename: "f504.jpg",
+    filesize: 48975,
+    title: "Desert",
+    description: "",
+    user_id: 3609,
+    media_type: "image/jpeg",
+    created_at: "2023-10-12T06:59:05.000Z",
+  },
+  {
+    media_id: 9590,
+    filename: "60ac.jpg",
+    filesize: 23829,
+    title: "Basement",
+    description: "Light setup in basement",
+    user_id: 305,
+    media_type: "image/jpeg",
+    created_at: "2023-10-12T06:56:41.000Z",
+  },
 ];
 
 /**
  * Gets all items
- * 
- * @param {object} req - http request 
+ *
+ * @param {object} req - http request
  * @param {object} res - http response
  */
 const getItems = (req, res) => {
   const limit = req.query.limit;
   // TODO: check that the param value is int before using
   if (limit) {
-    res.json(items.slice(0, limit));
+    res.json(mediaItems.slice(0, limit));
   } else {
-    res.json(items);
+    res.json(mediaItems);
   }
 };
 
 /**
- * 
- * @param {*} req 
- * @param {*} res 
+ *
+ * @param {*} req
+ * @param {*} res
  */
 const getItemsById = (req, res) => {
   // if item with id exists send it, otherwise send 404
-  console.log('getItemsById', req.params);
-  const item = items.find((element) => element.id == req.params.id);
+  console.log("getItemsById", req.params);
+  const item = mediaItems.find((element) => element.id == req.params.id);
   if (item) {
     res.json(item);
   } else {
     res.status(404);
-    res.json({message: "Item not found."});
+    res.json({ message: "Item not found." });
   }
 };
 
 const postItem = (req, res) => {
-  console.log('new item posted', req.body);
+  console.log("new item posted", req.body);
   // TODO: check last weeks example for generating an id
   if (req.body.name) {
-    items.push({id: 0, name: req.body.name});
+    mediaItems.push({ id: 0, name: req.body.name });
     res.sendStatus(201);
   } else {
     res.sendStatus(400);
   }
 };
 
-// TODO: add deleteItem(), putItem() and routing for those in index.js
+const deleteItem = (res, id) => {
+  const index = mediaItems.findIndex((item) => item.id == id);
+  if (index !== -1) {
+    mediaItems.splice(index, 1);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(`{"message": "Item with id ${id} deleted."}`);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end('{"message": "Item not found."}');
+  }
+};
 
-export {getItems, getItemsById, postItem};
+const putItem = (req, res, id) => {
+  let body = [];
+  req
+    .on("error", (err) => {
+      console.error(err);
+    })
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log("req body", body);
+      body = JSON.parse(body);
+      if (!body.name) {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(`{"message": "Missing data."}`);
+        return;
+      }
+      const index = mediaItems.findIndex((item) => item.id == id);
+      if (index !== -1) {
+        mediaItems[index].name = body.name;
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(`{"message": "Item with id ${id} updated."}`);
+      } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end('{"message": "Item not found."}');
+      }
+    });
+};
+
+export { getItems, getItemsById, postItem, deleteItem, putItem };
